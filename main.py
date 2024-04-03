06cd7fc1dfbf03d848f213606c9ca0a63a305ff1
gostash <26434394-gostash@users.noreply.replit.com>
2024-04-03 11:23:22 +0000

    Я создал объект класса Table с заданными колонками "a" и "b". После выполненных операций объект Table  будет содержать две строки данных со словорями fLine и sLine.

--- main.py
+++ main.py
@@ -4,22 +4,33 @@ class Table:
     self.data = []
 
   def add_row(self, row):
+    # TODO - добавить проверку на наличие названия колонки!
     self.data.append(row)
 
   def __bool__(self):
     return len(self.data) > 0
     
   def __str__(self):
-    table_str = "" 
+    table_str = " " 
+    #Преобразование данных таблицы в строку
     table_str += " ".join(self.columns) + "\n"
-for row in self.data:
-  row_str = "".join([str(col]) for col in self.columns])
-  table_str += row_str + "\n"
-  return table_str
+    for row in self.data:
+    # ЭТО препод пишет!!!
+        row_str = "".join([str(col]) for col in self.columns])
+        table_str += row_str + "\n"
+    return table_str
+
+# Создание таблицы с заданными колонками
+table = Table(["a", "b"])
+fLine = {"a":2, "b":4}
+sLine = {"a":3, "b":9}
+table.add_row(fLine)
+table.add_row(sLine)
+
 
   
   
-  
+  # У нас проверка в конце будет
   
   
   #fLine = {"a" : 2, "b" : 4}
