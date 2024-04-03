96056eaa8468948fc41b224c7970aaa27774e13f
aigulnas <152395148+aigulnas@users.noreply.github.com>
2024-04-03 10:41:07 +0000

    Добавление новой строки данных в таблицу данных класса Table

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ class Table:
   def __init__(self, columns):
     self.columns = columns
     self.data = []
+
+  def add_row(self, row):
+    self.data.append(row)
       
 #fLine = {"a" : 2, "b" : 4}
 #sLine = {"a" : 3, "b" : 9}
