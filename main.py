eb61822ebe636657f82c6fdc57331e2fccbdf80a
yyourmaryy <26434418-marydenisovna@users.noreply.replit.com>
2024-04-03 10:55:24 +0000

    Добавила функцию bool(...) для возвращения True или False

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ class Table:
 
   def add_row(self, row):
     self.data.append(row)
-      
+
+  def __bool__(self):
+    return len(self.data) > 0
+    
+  def __str__(self):
+    table_str = "" 
+
+    
 #fLine = {"a" : 2, "b" : 4}
 #sLine = {"a" : 3, "b" : 9}
